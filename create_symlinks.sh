4559018016c26230c87379c7baba479607aebdba
Andrew-v-C <andrewvancampen@gmail.com>
2026-08-02 21:52:36 -0400

    Added fonts

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 # Shell (Bash)
-rm ~/.bash_profile
-rm ~/.profile
-rm ~/.bashrc
-ln -s ~/.dotfiles/shell/bash_profile.sh ~/.bash_profile
-ln -s ~/.dotfiles/shell/bashrc.sh ~/.bashrc
+ln -sf ~/.dotfiles/shell/bash_profile.sh ~/.bash_profile
+ln -sf ~/.dotfiles/shell/bashrc.sh ~/.bashrc
 
 # Git
-rm ~/.gitconfig
-rm -rf ~/.config/git
-ln -s ~/.dotfiles/git ~/.config/git
+ln -sfn ~/.dotfiles/git ~/.config/git
 
 # Alacritty
-rm -rf ~/.config/alacritty
-ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -sfn ~/.dotfiles/alacritty ~/.config/alacritty
 
 # Neovim
-rm -rf ~/.config/nvim
-ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -sfn ~/.dotfiles/nvim ~/.config/nvim
 
 # GitHub Copilot
-rm -rf ~/.copilot
-ln -s ~/.dotfiles/copilot ~/.copilot
+ln -sfn ~/.dotfiles/copilot ~/.copilot
+
+# Fonts
+for dir in ~/.dotfiles/fonts/*/; do
+    ln -sfn "$dir" ~/.local/share/fonts/$(basename "$dir")
+done
